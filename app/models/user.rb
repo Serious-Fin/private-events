3fbb2b784a49ac31a6fb8f7ff6650e3244222407
Serious-Fin <augustasgri@gmail.com>
2023-06-03 11:13:02 +0300

    Add username to sign up page and make event show page dynamic

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   has_many :events, foreign_key: :creator_id, class_name: "Event"
 end
